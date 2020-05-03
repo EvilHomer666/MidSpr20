f516f5bb7e5b02e22f55799e502d35f5a1866d89
Edgar Espino <edgar.espino@loop.colum.edu>
2020-05-03 00:39:48 -0500

    Added scoreboard UI

---
+++ Assets/Scripts/UI/Scoreboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO; // << Using system.OI for input output
+
+// Custom name space not inheriting from Misbehavior
+namespace Game.ScoreboardData
+{
+public class Scoreboard : MonoBehaviour
+    {
+        [SerializeField] private int maxScoreEntries = 8;
+    }
+}
---
+++ Assets/Scripts/UI/ScoreboardEntryUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Custom name space not inheriting from Misbehavior
+namespace Game.ScoreboardData
+
+{
+    public class ScoreboardEntryUI : MonoBehaviour
+    {
+        [SerializeField] private Text entryNameText = null;
+        [SerializeField] private Text entryScoreText = null;
+        [SerializeField] private Text entryTimeText = null;
+
+        // Custom method to pass data
+        public void Initialize(ScoreboardEntryData scoreboardEntryData)
+        {
+            entryNameText.text = scoreboardEntryData.entryName;
+            entryScoreText.text = scoreboardEntryData.entryScore.ToString(); // << Convert to string to display on the screen
+            entryTimeText.text = scoreboardEntryData.entryTime.ToString(); // << Convert to string to display on the screen
+        }
+
+    }
+}
